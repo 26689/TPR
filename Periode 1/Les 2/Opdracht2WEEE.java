dccbd7e9a6f473cba8b62e33c21c5d225bcd040f
MA-Erik <e.vanwees@ma-web.nl>
2017-10-11 19:06:36 +0200

    opdracht2_upload

---
+++ Periode 1/Les 2/Opdracht2WEEE.java
@@ -0,0 +1,124 @@
+import java.awt.Label;
+
+public class Opdracht2WEEE
+{
+
+	public static void main(String[] args)
+	{
+		
+		// MAAK ALLE ONDERDELEN AF OM DE OPDRACHT TE VOLTOOIEN!
+		
+		OnderdeelEen();
+		OnderdeelTwee();
+		OnderdeelDrie();
+		
+	}
+	
+	
+	public static void OnderdeelEen()
+	{
+		
+		/*
+		 * ################## VARIABELEN MAKEN ##################
+		 * 
+		 * Maak twee variabelen en print de waarde van deze variabelen uit.
+		 * De eerste variabele moet een "boolean" zijn en de tweede variabele
+		 * moet een "String" zijn. (Zorg ervoor dat de String gevuld is. ;D )
+		 * 
+		 */
+		
+		System.out.println("################## VARIABELEN MAKEN ##################");
+		
+		
+		// Maak hier de variabelen:
+		
+		
+		
+		// VERVANG DE COMMENT (inclusief /* en */ ) MET JOUW VARIABELEN!
+		
+		System.out.println(  /* HIER KOMT VARIABELE A */  );
+		System.out.println(  /* HIER KOMT VARIABELE B */  );
+		
+		
+	}
+	
+	public static void OnderdeelTwee()
+	{
+		
+		/*
+		 * ################## DATATYPES ##################
+		 * 
+		 * Zorg ervoor dat de correcte soort waarde in de getypeerde variabelen terecht komt.
+		 * 
+		 */
+		
+		// Vul de correcte waarde bij deze variabelen in:
+		
+		String	variabeleA;
+		boolean variabeleB;
+		int		variabeleC;
+		
+		Label	variabeleD = new Label("Naam van het Label"); // Deze klopt al.
+		
+		char	variabeleE;
+		double	variabeleF;
+		float	variabeleG;
+		
+		
+		
+		// Hier worden de resultaten geprint:
+		
+		System.out.println("################## DATATYPES ##################");
+		System.out.println(variabeleA);
+		System.out.println(variabeleB);
+		System.out.println(variabeleC);
+		
+		System.out.println(variabeleD.getText());
+		
+		System.out.println(variabeleE);
+		System.out.println(variabeleF);
+		System.out.println(variabeleG);
+		
+	}
+	
+	public static void OnderdeelDrie()
+	{
+		
+		/*
+		 * ################## VARIABELEN IN VARIABELEN ##################
+		 * 
+		 * Maak 8 variabelen in totaal, waarvan 2 een "boolean" zijn, 3 een "int" en 3 een "String".
+		 * 
+		 * Boolean 1 heeft een ja/nee waarde.
+		 * Boolean 2 heeft een referentie naar boolean 1.
+		 * 
+		 * Int 1 heeft een getallenwaarde.
+		 * Int 2 heeft een getallenwaarde.
+		 * Int 3 heeft de waarde: int 1 - int 2. (int 1 MINUS int 2)
+		 * 
+		 * String 1 heeft een tekstwaarde.
+		 * String 2 heeft een tekstwaarde.
+		 * String 3 heeft de waarde: String 1 + String 2. (String 1 PLUS String 2)
+		 * 
+		 * 
+		 * Vervolgens print je boolean 2, int 3 en String 3 uit.
+		 * 
+		 */
+		
+		System.out.println("################## VARIABELEN IN VARIABELEN ##################");
+		
+		
+		
+		// Maak hier de variabelen:
+		
+		
+		
+		// VERVANG DE COMMENT (inclusief /* en */ ) MET JOUW VARIABELEN!
+		
+		System.out.println(  /* HIER KOMT BOOLEAN 2 */  );
+		System.out.println(  /* HIER KOMT INT 3 */  );
+		System.out.println(  /* HIER KOMT STRING 3 */  );
+		
+	}
+
+}
